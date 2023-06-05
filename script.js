c4e4ff530e5c431382945e9e32d045eb4b84e0b5
veena-7 <92582138+veena-7@users.noreply.github.com>
2023-06-05 21:40:47 +0530

    first DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("BZ")
+var data1 = {image:"abc.jpg",name: "Yerraguntla Veena",designation:"Student"};
+var data2 = {image:"bcd.jpg",name: "Y Veena",designation:"Intern"};
+var isdata1 = true;
+var displaydata;
+var newpic=function(){
+   if(isdata1) {
+    displaydata=data2;
+    isdata1=false;
+   }
+   else{
+    displaydata=data1;
+    isdata1=true;
+   }
+   document.getElementById("mi").src=displaydata.image;
+   
+}
