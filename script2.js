d785477aa68fa77732fdf8429a0342f88e9f4f32
veena-7 <92582138+veena-7@users.noreply.github.com>
2023-06-06 22:02:45 +0530

    fixing errors

--- script2.js
+++ script2.js
@@ -1,6 +1,7 @@
 var userinfo={name:"",image:"",username:""};
 var newpic=function(){
-    fetch("new.json").then((response) => response.json()).then((data) =>{ userinfo.name=data.results[0].name.first;
+    fetch("new.json").then((response) => response.json()).then((data) =>{ 
+        userinfo.name=data.results[0].name.first;
         userinfo.image=data.results[0].picture.large;
         userinfo.username=data.results[0].login.username;
         document.getElementById("mi").src=userinfo.image;
